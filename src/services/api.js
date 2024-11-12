d9f96a1728b2d6d1ef24e5266920c958cc67ab78
Phan Trương Quý Hòa <93481453+quyhoaphantruong@users.noreply.github.com>
2024-11-12 19:30:16 +0700

    change url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://ia04-jwtauthentication-backend.onrender.com',
 });
 
 api.interceptors.request.use(
